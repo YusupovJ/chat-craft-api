102086140cf2a48822910e3031bf3dc171a8f748
YusupovJ <jamshudanamana@gmail.com>
2024-08-10 16:22:18 +0500

    change linter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { dbConfig } from "./config/db.config";
+import { ChatModule } from './modules/chat/chat.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbConfig)],
+  imports: [TypeOrmModule.forRoot(dbConfig), ChatModule],
 })
 export class AppModule {}
