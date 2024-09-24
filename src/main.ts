3d96c1912e200b14e51638528da88d4eb192f73b
YusupovJ <jamshudanamana@gmail.com>
2024-09-24 16:59:02 +0500

    add samalety

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { envConfig } from "./config/env.config";
 import { ValidationPipe } from "@nestjs/common";
-import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(envConfig.port);
+  console.log(`🚀🚀🚀 Listening on port ${envConfig.port} 🚀🚀🚀`);
 }
 
 bootstrap();
